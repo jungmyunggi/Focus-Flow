793939d1cc9c06e573fc86478794dd40a12b0433
정명기 <jmk101711@yu.ac.kr>
2025-03-04 15:19:32 +0900

    📦 chore: 초기 환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-primary">Focus Flow</p>
+      <p className="text-secondary">test</p>
+      <p className="text-accent">test</p>
+    </>
+  );
+}
+
+export default App;
